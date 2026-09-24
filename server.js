5f1348e2873eceddd737cf43ed872a57b1ad1aef
jpmayores <jhonphilipmayores@gmail.com>
2026-09-24 13:08:42 +0800

    Increase body parser payload limit to 50mb

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const db = mysql.createPool({
